12718eb1ec59346141c42ba60a47b6ac2dd0e622
Thomas Kulka <t.kulka@adnymics.com>
2020-04-15 12:11:45 +0200

    Rock Paper Scissors - small game in python

---
+++ python-projects/rock-paper-scissors/rock-paper-scissors.py
@@ -0,0 +1,38 @@
+import random
+
+options = ['rock', 'paper', 'scissors']
+
+user_selection = None
+AI_selection = random.choice(options)
+
+print("Select: rock .:|:. paper .:|:. scissors")
+
+# Make sure user types correct value
+while user_selection == None:
+  # raw_input is python 2.7.x function
+  user_selection = raw_input('Your pick: ').lower()
+  
+  if user_selection not in options:
+	print('Select from available options!')
+	user_selection = None
+
+# logic for different combinations
+if user_selection == AI_selection:
+  print('The game is drawn.')
+elif user_selection == 'rock':
+  if AI_selection == 'paper':
+    print('AI wins.')
+  else:
+    print('You won!')
+elif user_selection == 'paper':
+  if AI_selection == 'scissors':
+    print('AI wins.')
+  else:
+    print('You won!')
+elif AI_selection == 'rock':
+  print('AI wins.')
+else:
+  print('You won!')
+	
+print('AI selected: {}'.format(AI_selection))
+print('You selected: {}'.format(user_selection))
